545766914c7f53362a97f76371d1203072902668
dot-lachryma110 <52806825+dot-lachryma110@users.noreply.github.com>
2019-12-22 21:40:53 -0800

    Add files via upload

---
+++ arduinoproj.cpp
@@ -0,0 +1,32 @@
+//#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string fn;
+	string ln;
+	string luck;
+	int pixel_guess;
+	
+	std::cout << "What is your first name? " << endl;
+	std::cin >> fn;
+	
+	std::cout << "What is your last name?" <<endl;
+	std::cin >> ln;
+	
+	std::cout << "Are you feeling lucky? " <<endl;
+	std::cin >> luck;	
+	
+	std::cout << "Guess the pixel on the 8x8 screen that is going to be incended." << "\n" << endl;
+	std::cin >> luck;	
+	
+	
+	return 0;
+}
+
+
+
+
